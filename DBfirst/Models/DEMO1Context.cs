ff597c90cc7f23aa503ee9b7e8ab3fc82e28ad61
gpaptechnologies <gpaptechnologies@gmail.com>
2022-04-23 22:07:41 +0530

    Add project files.

---
+++ DBfirst/Models/DEMO1Context.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DBfirst.Models
+{
+    public partial class DEMO1Context : DbContext
+    {
+        public DEMO1Context()
+        {
+        }
+
+        public DEMO1Context(DbContextOptions<DEMO1Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employees { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                //                optionsBuilder.UseSqlServer("Server=MYPC\\SQLDEMO;Database=DEMO1;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.ToTable("Employee");
+
+                entity.Property(e => e.FirstName)
+                    .HasMaxLength(150)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ DBfirst/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace DBfirst.Models
+{
+    public partial class Employee
+    {
+        public int Id { get; set; }
+        [DisplayName("First Name")]
+        public string FirstName { get; set; } = null!;
+        [DisplayName("Last Name")]
+        public string? LastName { get; set; }
+    }
+}
